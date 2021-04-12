abc2c400f0fcaba418d9431a10fad90bed3743d3
davidsoto0 <73496986+davidsoto0@users.noreply.github.com>
2021-04-12 22:25:38 +0200

    Initial commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+import * as bootstrap from 'bootstrap'
+
+(function () {
+  const estudiante = "Gregorio David Soto Alcázar";
+  console.log(`Currículum Vítae de:,${estudiante}`);
+})();
